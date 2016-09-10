803f680904ea57701433622f8869fcad85259030
Vitor Baptista <vitor@vitorbaptista.com>
2016-09-10 14:02:05 +0100

    Fixing build

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import subprocess
 import json
 
@@ -9,6 +10,7 @@ class TestCLI(object):
         data = {
             'foo': 'bar',
         }
+        expected_output = _universal_newlines(json.dumps(data, indent=4))
         json_file = tmpdir.join('data.json')
         json_file.write(json.dumps(data))
         path = str(json_file.realpath())
@@ -16,7 +18,7 @@ class TestCLI(object):
         subprocess.call(['jsonformat', path])
 
         out, err = capfd.readouterr()
-        assert out == ('%s\n' % json.dumps(data, indent=4))
+        assert _universal_newlines(out.strip()) == expected_output
         assert not err
 
     def test_inexistent_file_raises_error(self, capfd):
@@ -25,11 +27,11 @@ class TestCLI(object):
 
         out, err = capfd.readouterr()
         expected_message = (
-            'Could not open file: {path}: No such file or directory\n'
+            'Could not open file: {path}: No such file or directory'
         ).format(path=path)
 
         assert not out
-        assert err.endswith(expected_message)
+        assert err.strip().endswith(expected_message)
         assert exit_code != 0
 
     def test_invalid_json_raises_error(self, capfd, tmpdir):
@@ -40,6 +42,14 @@ class TestCLI(object):
         exit_code = subprocess.call(['jsonformat', path])
 
         out, err = capfd.readouterr()
+        expected_message = (
+            'Error: {path} is not a valid JSON'
+        ).format(path=path)
+
         assert not out
-        assert err == 'Error: {path} is not a valid JSON\n'.format(path=path)
+        assert err.strip() == expected_message
         assert exit_code != 0
+
+
+def _universal_newlines(string):
+    return string.replace(os.linesep, '\n')
